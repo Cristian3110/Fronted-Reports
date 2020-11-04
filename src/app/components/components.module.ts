208a37d7701db992e6e104fd2ff5467c575117a3
cristian3110 <cristiancarrillo311087@gmail.com>
2020-11-04 16:29:21 -0400

    creación y configuración de componentes y módulos

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -2,6 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
+import { SlidesComponent } from './slides/slides.component';
+import { FormlineComponent } from './formline/formline.component';
+import { ForminterComponent } from './forminter/forminter.component';
+import { IonicModule } from '@ionic/angular';
 
 
 
@@ -9,9 +13,22 @@ import { HeaderComponent } from './header/header.component';
   declarations: [
     FooterComponent,
     HeaderComponent,
+    FooterComponent,
+    SlidesComponent,
+    FormlineComponent,
+    ForminterComponent
     ],
   imports: [
-    CommonModule
+    CommonModule,
+    IonicModule
+  ],
+  exports:[
+    FooterComponent,
+    HeaderComponent,
+    FooterComponent,
+    SlidesComponent,
+    FormlineComponent,
+    ForminterComponent
   ]
 })
 export class ComponentsModule { }
