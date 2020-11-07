ad1396cf47c6c287af745dbc926399ab9f3e5c63
cristian3110 <cristiancarrillo311087@gmail.com>
2020-11-07 04:58:15 -0400

    colocando estilo transparente al slides e importando el reactiveForm en el módulo de login

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -9,13 +10,54 @@ export class LoginPage implements OnInit {
 
   signupView: boolean = false;
 
-  constructor() { }
+  forma: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {
+
+    this.crearFormulario();
+   }
 
   ngOnInit() {
   }
 
   toggleSignUpView () {
     this.signupView = !this.signupView
+  };
+
+  get emailNoValido(){
+    return this.forma.get('email').invalid && this.forma.get('email').touched
+  }
+
+  get passwordNoValido(){
+    return this.forma.get('password').invalid && this.forma.get('password').touched
+  }
+
+  crearFormulario(){
+
+    this.forma = this.formBuilder.group({
+      email:['',[Validators.required, Validators.pattern('^[a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,4}$')]],
+      password:['', Validators.required],
+    });
+
+  }
+
+  async loginForm(event: Event){
+    const datos = this.forma.value;
+    console.log(this.forma.valid);
+    console.log(this.forma.value);
+  
+    const valido = await this.usuarioService.login(datos.email, datos.password);   
+    
+    if(valido){
+      // navegar a la pantalla siguiente con el navCtrl no se puede devolver hasta que se loguee
+      this.navCtrl.navigateRoot('/servicios', {animated:true});
+    }else{
+      //mostrar alerta de usuario y contraseña no son correctos
+      this.uiService.alertaInformativa(
+        'usuario y constraseña no son correctos'
+      );
+    }
+
   };
 
 }
