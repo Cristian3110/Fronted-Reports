9c444e9a275e77a491bb66cbb0c80fa92b5e06a3
cristian3110 <cristiancarrillo311087@gmail.com>
2020-11-14 01:04:08 -0400

    diseño de la page de servicios

--- src/app/pages/servicios/servicios.page.ts
+++ src/app/pages/servicios/servicios.page.ts
@@ -8,6 +8,19 @@ import { ReportsService } from '../../services/reports.service';
 })
 export class ServiciosPage implements OnInit {
 
+  slides: { img: string, titulo: string, desc: string }[] = [
+    {
+      img: '/assets/phone.svg',
+      titulo: 'Reportes de Lineas Telefónicas',
+      desc: 'Seleccionando en el botón de LINEA, puedes consultar o crear tu reporte'
+    },
+    {
+      img: '/assets/network.svg',
+      titulo: 'Reportes de Internet Aba',
+      desc: 'Seleccionando en el botón de INTERNET, puedes consultar o crear tu reporte'
+    }
+  ];
+
   constructor(private reportsService: ReportsService) { }
 
   ngOnInit() {
